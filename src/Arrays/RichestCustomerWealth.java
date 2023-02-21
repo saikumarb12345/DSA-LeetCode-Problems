0d735c33c5b59b2524290f592a30f46e9ab398cf
SaiKumar <bsainaidu90@gmail.com>
2023-02-21 22:16:34 +0530

    21-02-2023 Arrays (LeetCode)

---
+++ src/Arrays/RichestCustomerWealth.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class RichestCustomerWealth {
+        public int maximumWealth(int[][] accounts) {
+            int max = 0;
+            for(int i=0;i<accounts.length;i++){
+                int sum = 0;
+                for(int j=0;j<accounts[i].length;j++){
+                    sum = sum + accounts[i][j];
+                }
+                if(sum > max){
+                    max = sum;
+                }
+            }
+            return max;
+        }
+}
