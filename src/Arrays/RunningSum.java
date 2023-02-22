a7b40d53bb0ac063cac251c41917962bfff82ca8
SaiKumar <bsainaidu90@gmail.com>
2023-02-22 22:53:44 +0530

    22-02-2023 Arrays (LeetCode)

---
+++ src/Arrays/RunningSum.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class RunningSum {
+    public static void main(String[] args) {
+        int[]  nums = {1,2,3,4};
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
+    public static int[] runningSum(int[] nums) {
+
+        for(int i=1;i<nums.length;i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
+    }
+}
